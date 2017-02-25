d04bb4fa33afa7a887b7e71572fde722cc8f9463
meganrm <meganrm@gmail.com>
2017-02-24 20:53:22 -0800

    changed view to made database

--- scripts/views/eventView.js
+++ scripts/views/eventView.js
@@ -279,7 +279,7 @@
   eventHandler.metaData = function(){
     metaDataObj = new TownHall();
     metaDataObj.topZeroResults = []
-    firebase.database().ref('/lastupdatedTesting/time').once('value').then(function(snapshot){
+    firebase.database().ref('/lastupdated/time').on('changed', function(snapshot){
       metaDataObj.time = new Date(snapshot.val())
       metaDataObj.total = TownHall.allTownHallsFB.length
       var topZeros = firebase.database().ref('zipZeroResults/').orderByValue().limitToLast(10);
@@ -299,7 +299,7 @@
   }
 
   eventHandler.readData = function (){
-    firebase.database().ref('/townHallsTesting/').on('child_added', function getSnapShot(snapshot) {
+    firebase.database().ref('/townHalls/').on('child_added', function getSnapShot(snapshot) {
       var ele = new TownHall (snapshot.val());
       var id = ele.eventId;
       TownHall.allTownHallsFB.push(ele)
