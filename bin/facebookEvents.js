ed5214edeead14a26d2b0242ff09e5e55bb508d0
meganr <meganrm@gmail.com>
2017-08-27 12:34:42 -0700

    troubleshooting heroku run

--- bin/facebookEvents.js
+++ bin/facebookEvents.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
+
+
+var admin = require('firebase-admin');
+var request = require('request-promise'); // NB:  This is isn't the default request library!
+
+var eventbriteToken = process.env.EVENTBRITE_TOKEN;
+var statesAb = require('../bin/stateMap.js');
+var firebasedb = require('../bin/setupFirebase.js');
+
+var moment = require('moment');
+
+
 console.log('working')
