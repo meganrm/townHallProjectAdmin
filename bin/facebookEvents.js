7c818007e214f5924b99b30973bc5ba019184f6a
meganr <meganrm@gmail.com>
2017-08-27 12:38:49 -0700

    troubleshooting heroku run

--- bin/facebookEvents.js
+++ bin/facebookEvents.js
@@ -2,7 +2,6 @@
 
 
 
-var admin = require('firebase-admin');
 var request = require('request-promise'); // NB:  This is isn't the default request library!
 
 var eventbriteToken = process.env.EVENTBRITE_TOKEN;
@@ -11,5 +10,62 @@ var firebasedb = require('../bin/setupFirebase.js');
 
 var moment = require('moment');
 
+// Get list of existing townhalls so we don't submit duplicates
+var existingTownHallIds = [];
+
+firebasedb.ref('/townHallIds/').once('value').then(function(snapshot){
+  snapshot.forEach(node => {
+    existingTownHallIds.push(node.val().eventId);
+  });
+  getTownhalls();
+});
+
+function getTownhalls() {
+  firebasedb.ref('mocData/').once('value').then((snapshot) => {
+    getFacebookEvents(snapshot.val());
+  });
+  // getEventbriteEvents();
+}
+
+function getFacebookEvents(MoCs) {
+  var facebookPromises = [];
+  var startDate = Math.floor(new Date() / 1000); //Needs to be in Unix timestamp form
+  Object.keys(MoCs).forEach(id => {
+    let MoC = MoCs[id];
+    if (MoC.hasOwnProperty('facebook') && MoC.facebook_account) {
+      facebookPromises.push(
+        createFacebookQuery(MoC.facebook_account, startDate).then(res => {
+          // Create references to MoCs for easy data lookup later
+          res.data.forEach(event => event.MoC = MoC);
+          return res.data;
+        }).catch(() => {})
+      );
+    }
+  });
+  Promise.all(facebookPromises).then(res => {
+    // Stop gap measure for while we have bad facebook id data and are getting undefined
+    res = res.filter(eventCollection => {
+      if (Array.isArray(eventCollection)) {
+        return eventCollection;
+      }
+    });
+    // Collapse into flat array
+    var facebookEvents = [].concat.apply([], res);
+    var newEventIds = removeExistingIds(facebookEvents.map(event => 'fb_' + event.id));
+
+    facebookEvents.forEach(event => {
+      if (newEventIds.indexOf('fb_' + event.id) !== -1) {
+        // submitTownhall(transformFacebookTownhall(event));
+      }
+    });
+  });
+}
+
+function createFacebookQuery(facebookID, startDate) {
+  return request({
+    uri: 'https://graph.facebook.com/v2.10/' + facebookID + '/events?since=' + startDate +'&access_token=' + facebookToken,
+    json: true
+  });
+}
 
 console.log('working')
