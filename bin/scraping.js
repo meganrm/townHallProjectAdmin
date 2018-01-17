61d86a5c8054998e4ff54853e8b13bee3fb45ee4
Paolo Unger Dvorchik <paolo@spry-group.com>
2018-01-17 00:36:36 -0500

    157 Create common scraping module

---
+++ bin/scraping.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+require('dotenv').load();
+
+function ScrapingModule() {};
+
+ScrapingModule.firebasedb = require('../server/lib/setupFirebase');
+
+ScrapingModule.getTownhalls = function() {
+  return new Promise(function(resolve, reject) {
+    // Get list of existing townhalls so we don't submit duplicates
+    var existingTownHallIds = [];
+
+    ScrapingModule.firebasedb.ref('/townHallIds/').once('value').then(function(snapshot){
+      snapshot.forEach(node => {
+        existingTownHallIds.push(node.val().eventId);
+      });
+
+      ScrapingModule.firebasedb.ref('mocData/').once('value').then((snapshot) => {
+        resolve ({
+            existingTownHallIds: existingTownHallIds,
+            MoCs: snapshot.val()
+        });
+      });
+    });
+  });
+}
+
+ScrapingModule.removeExistingIds = function(existingTownHallIds, eventIds) {
+  existingTownHallIds.forEach(existingId => {
+    let position = eventIds.indexOf(existingId);
+    if (position !== -1) {
+      eventIds.splice(position, 1);
+    }
+  });
+  return eventIds;
+}
+
+ScrapingModule.unqiueFilter = function(value, index, self) {
+  return self.findIndex(obj => obj.id === value.id) === index;
+}
+
+ScrapingModule.submitTownhall = function(townhall) {
+  var updates = {};
+  updates['/townHallIds/' + townhall.eventId] = {
+    eventId: townhall.eventId,
+    lastUpdated: Date.now(),
+  };
+  updates['/UserSubmission/' + townhall.eventId] = townhall;
+  return ScrapingModule.firebasedb.ref().update(updates);
+}
+
+module.exports = ScrapingModule;
