2d06ab7efcd1f13683659acad1990982ec5c4854
meganr <meganrm@gmail.com>
2018-01-26 20:18:48 -0800

    updated propublica request

--- bin/weeklyPropublica.js
+++ bin/weeklyPropublica.js
@@ -1,114 +1,41 @@
 #!/usr/bin/env node
+const request = require('superagent');
+const firebasedb = require('../server/lib/setupFirebase.js');
+const ErrorReport = require('../server/lib/errorReporting.js');
+const Moc = require('../server/moc');
 
+const propublicaAPI = process.env.PROPUBLICA;
 
-var firebasedb = require('../server/lib/setupFirebase.js');
-var ErrorReport = require('../bin/errorReporting.js');
-
-function WeeklyPropublicaUpdate(opts) {
-  for (let keys in opts) {
-    this[keys] = opts[keys];
-  }
+function getNewMembers() {
+  let url = 'https://api.propublica.org/congress/v1/members/new.json';
+  return request
+    .get(url)
+    .set('X-API-Key', propublicaAPI)
+    .then((res) => {
+      try {
+        let data = JSON.parse(res.text);
+        return data.results[0].members;
+      } catch (e) {
+        console.log(e);
+      }
+    });
 }
 
-var https = require('https');
-var statesAb = require('../server/data/stateMap.js');
-
-// check eventbrite accounts need to be parsed as #'s
-function Moc(opts) {
-  if (!opts.member_id) {
-    return;
-  }
-  for (let keys in opts) {
-    this[keys] = opts[keys];
-  }
-  this.propublica_id = opts.member_id;
-  this.propublica_facebook = opts.facebook_account;
-  if (parseInt(this.propublica_facebook)) {
-    this.propublica_facebook = parseInt(this.propublica_facebook);
-  }
-  if (opts.current_party && opts.current_party.toLowerCase() === 'd') {
-    this.party = 'Democratic';
-  } else if (opts.current_party && opts.current_party.toLowerCase() === 'r') {
-    this.party = 'Republican';
-  } else if (opts.current_party) {
-    this.party = 'Independent';
-  }
-  if (opts.state) {
-    this.stateName = statesAb[opts.state];
-  }
-  delete this.roles;
-  delete this.facebook_account;
-  delete this.member_id;
-  delete this.current_party;
+function getSpecificMember(url) {
+  return request
+    .get(url)
+    .set('X-API-Key', propublicaAPI)
+    .then((res) => {
+      try {
+        let data = JSON.parse(res.text);
+        return data.results[0];
+      } catch (e) {
+        console.log(e);
+      }
+    });
 }
 
-//   function weeklyPropublicaUpdate() {
-function loadNewMembersRequest() {
-  return new Promise(function(resolve, reject) {
-    var options = {
-      hostname: 'api.propublica.org',
-      path: '/congress/v1/members/new.json',
-      headers: {
-        'X-API-Key': 'CGreQp3d95C4FLYHkCZRph5Hhs9nqfRCdJNlrxHL',
-      },
-      method: 'GET',
-    };
-
-    const req = https.request(options, res => {
-      res.on('data', d => {
-        // process.stdout.write(d);
-        var r = JSON.parse(d);
-        if (r['results'][0]['members']) {
-          resolve(r['results'][0]['members']);
-        } else {
-          reject('no data came back from propublica/new');
-        }
-      });
-    });
-
-    req.on('error', e => {
-      console.error('error on request', e);
-    });
-    req.end();
-  });
-}
-
-function findSpecificMemberRequest(member_id) {
-  return new Promise(function(resolve, reject) {
-    var options = {
-      hostname: 'api.propublica.org',
-      path: '/congress/v1/members/' + member_id + '.json',
-      headers: {
-        'X-API-Key': 'CGreQp3d95C4FLYHkCZRph5Hhs9nqfRCdJNlrxHL',
-      },
-      method: 'GET',
-    };
-    var str = '';
-    const req = https.request(options, res => {
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => {
-        str += chunk;
-      });
-      res.on('end', () => {
-        var r = JSON.parse(str);
-        var member = r['results'][0];
-        if (member) {
-          resolve(member);
-        } else {
-          reject('no data came back from propublica/members');
-        }
-      });
-    });
-
-    req.on('error', e => {
-      console.error('error getting ', e);
-      reject(e);
-    });
-    req.end();
-  });
-}
-
-function updateNewMembers(newPropublicaMembers) {
+function updateDatabaseWithNewMembers(newPropublicaMembers) {
   newPropublicaMembers.forEach(function(new_propub_member) {
     var type;
     if (new_propub_member.chamber == 'House') {
@@ -117,72 +44,40 @@ function updateNewMembers(newPropublicaMembers) {
       type = 'sen';
     }
     // check against propublica specific member search using id
-    findSpecificMemberRequest(new_propub_member.id)
-      .then(function(existing_propub_member) {
-        // console.log(existing_propub_member);
-        var member = new Moc(existing_propub_member);
-        member.type = type;
-        // check mocData for matches
-        var path = '/mocData/' + existing_propub_member.govtrack_id;
+    getSpecificMember(new_propub_member.api_uri)
+      .then(function(fullPropPublicaMember) {
+        var newMember = new Moc(fullPropPublicaMember);
+        newMember.type = type;
+        if (!fullPropPublicaMember.govtrack_id) {
+          return console.log('no govtrack_id', fullPropPublicaMember.member_id);
+        }
+        var path = '/mocData/' + fullPropPublicaMember.govtrack_id;
         firebasedb.ref(path).once('value').then(function(snapshot) {
           if (!snapshot.exists()) {
-            // if no match
-            // update an entirely new member
-            member.displayName = member.first_name + ' ' + member.last_name;
-            member.state = new_propub_member.state;
-            member.district = new_propub_member.district;
-            member.stateName = statesAb[new_propub_member.state];
-            // firebasedb.ref(path).update(member).then(function(done){
-            //   console.log(done);
-            // });
-            var lastname = member.last_name.replace(/\W/g, '');
-            var firstname = member.first_name.replace(/\W/g, '');
-            var memberKey = lastname.toLowerCase() + '_' + firstname.toLowerCase();
-            var mocIDPath = '/mocID/' + memberKey;
-            var memberIDObject = {
-              id : member.govtrack_id,
-              nameEntered: member.displayName,
-            };
-            // ADD NEW MEMBER INFO TO mocID/
-            firebasedb.ref(mocIDPath).update(memberIDObject)
-            .then(function(done){
-              console.log('Added member to mocID/ endpoint ' + done);
-            }).catch(function(error){
-              let errorEmail = new ErrorReport(member.govtrack_id + ':' + error, 'Could not save a new mocID');
-              errorEmail.sendEmail('Megan Riel-Mehan <meganrm@townhallproject.com>');
-            });
-          } else {
-            // if match - update only fields that may change (social media)
-            console.log('existing member', member.govtrack_id);
-            firebasedb.ref(path).update(member).then(function(done){
-              console.log(done);
-            }).catch(function(error){
-              let errorEmail = new ErrorReport(member.govtrack_id + ':' + error, 'Could not find propublica member');
-              errorEmail.sendEmail('Megan Riel-Mehan <meganrm@townhallproject.com>');
-            });
+            return newMember.createNew(fullPropPublicaMember);
           }
-          console.log('---------');
+          return newMember.update(path)
+          .then(function(){
+            console.log('done');
+          }).catch(function(error){
+            let errorEmail = new ErrorReport(newMember.govtrack_id + ':' + error, 'Could not find propublica member');
+            errorEmail.sendEmail('Megan Riel-Mehan <meganrm@townhallproject.com>');
+          });
         });
       })
       .catch(function(error) {
-        let errorEmail = new ErrorReport(member.govtrack_id + ':' + error, 'Could not update existing moc');
+        let errorEmail = new ErrorReport(new_propub_member.id + ':' + error, 'Could not update existing moc');
         errorEmail.sendEmail('Megan Riel-Mehan <meganrm@townhallproject.com>');
       });
   });
 }
 
-WeeklyPropublicaUpdate.getUpdate = function() {
   // call propublica 'new members' api endpoint
-  loadNewMembersRequest()
+getNewMembers()
     .then(function(newMembers) {
       console.log('got all new members');
-      updateNewMembers(newMembers);
+      updateDatabaseWithNewMembers(newMembers);
     })
     .catch(function(error) {
       console.log('Uh oh, something went wrong getting new members ', error);
     });
-};
-
-WeeklyPropublicaUpdate.getUpdate();
-
-module.exports = WeeklyPropublicaUpdate;
---
+++ server/moc/index.js
@@ -0,0 +1,63 @@
+const firebasedb = require('../lib/setupFirebase.js');
+var statesAb = require('../data/stateMap.js');
+
+class Moc {
+  constructor(opts) {
+    if (!opts.member_id) {
+      return;
+    }
+    for (let keys in opts) {
+      this[keys] = opts[keys];
+    }
+    this.propublica_id = opts.member_id;
+    this.propublica_facebook = opts.facebook_account;
+    if (parseInt(this.propublica_facebook)) {
+      this.propublica_facebook = parseInt(this.propublica_facebook);
+    }
+    if (opts.current_party && opts.current_party.toLowerCase() === 'd') {
+      this.party = 'Democratic';
+    } else if (opts.current_party && opts.current_party.toLowerCase() === 'r') {
+      this.party = 'Republican';
+    } else if (opts.current_party) {
+      this.party = 'Independent';
+    }
+    if (opts.state) {
+      this.stateName = statesAb[opts.state];
+    }
+    delete this.roles;
+    delete this.facebook_account;
+    delete this.member_id;
+    delete this.current_party;
+  }
+
+  createNew(newPropublicaMember) {
+    let updates = {};
+    this.displayName = this.first_name + ' ' + this.last_name;
+    this.state = newPropublicaMember.state;
+    this.district = newPropublicaMember.district;
+    this.stateName = statesAb[newPropublicaMember.state];
+
+    const lastname = this.last_name.replace(/\W/g, '');
+    const firstname = this.first_name.replace(/\W/g, '');
+    const memberKey = lastname.toLowerCase() + '_' + firstname.toLowerCase();
+
+    const memberIDObject = {
+      id : this.govtrack_id,
+      nameEntered: this.displayName,
+    };
+
+    updates['/mocData/' + this.govtrack_id] = this;
+    updates['/mocID/' + memberKey] = memberIDObject;
+    console.log('updates', '/mocData/' + this.govtrack_id, this.displayName);
+    return Promise.resolve();
+    // return firebasedb.ref().update(updates);
+  }
+
+  update(path) {
+    // if match - update only fields that may change (social media)
+    console.log('existing member', this.govtrack_id);
+    return firebasedb.ref(path).update(this);
+  }
+}
+
+module.exports = Moc;
