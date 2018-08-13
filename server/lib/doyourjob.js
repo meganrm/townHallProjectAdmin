c1438edfeb6282c7a3b8a32bc6b3f0b72f63e68a
meganrm <meganrm@gmail.com>
2018-08-13 15:54:19 -0700

    updates to do your job check

--- server/lib/doyourjob.js
+++ server/lib/doyourjob.js
@@ -11,9 +11,23 @@ const getMMs = () => {
             let missingMembers = [];
             snapshot.forEach((ele) => {
                 let moc = ele.val();
-                console.log(moc.displayName, moc.govtrack_id, moc.next_election);
-                if (moc.missingMember && moc.in_office && moc.next_election === '2018') {
-                    missingMembers.push({state: moc.state, district: moc.district, type: moc.type});
+                if (moc.missingMember === "true"){
+                    moc.missingMember = true;
+                }
+                if (moc.missingMember === "false") {
+                    moc.missingMember = false;
+                }
+                if (moc.next_election && moc.next_election !== '2018') {
+                    return;
+                }
+                if (moc.missingMember && moc.in_office && !moc.retiring) {
+                    const mmDistrict = {
+                        state: moc.state,
+                        district: moc.district,
+                        type: moc.type,
+                        mmName:  moc.displayName,
+                    };
+                    missingMembers.push(mmDistrict);
                 }
             });
             return missingMembers;
@@ -23,12 +37,14 @@ const writeOut = (mm, displayName) => {
     writeStream.write([
         mm.state,
         mm.district || 'Senate',
+        mm.mmName,
         displayName || 'none',
 
     ].join(',') + '\n');
 };
 
 const getPledgers = (mmArray) => {
+    console.log(mmArray)
     mmArray.forEach(mm => checkPledger(mm));
 };
 
@@ -36,14 +52,19 @@ const checkPledger = (mm) => {
     return firebasedb.ref(`town_hall_pledges/${mm.state}`)
         .once('value')
         .then(pledgers => {
+
             pledgers.forEach(ele => {
                 let pledger = ele.val();
                 if (!pledger.pledged) {
                     return;
                 }
-                if (pledger.status !== 'Active Primary Candidate' && pledger.status !== 'Nominee' && !pledger.incumbent) {
+                if (pledger.status !== 'Nominee') {
                     return;
                 }
+                if (pledger.incumbent) {
+                    return;
+                }
+
                 if (mm.district && pledger.district) {
                     if (Number(mm.district) === Number(pledger.district)) {
                         return writeOut(mm, pledger.displayName);
