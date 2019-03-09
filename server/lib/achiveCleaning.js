ac961977a25757e413300259b442985bbd89989c
meganrm <meganrm@gmail.com>
2019-03-08 22:50:05 -0800

    seperated out by congressional session

--- server/lib/achiveCleaning.js
+++ server/lib/achiveCleaning.js
@@ -1,8 +1,12 @@
 const uniq = require('lodash').uniq;
 const includes = require('lodash').includes;
-const moment = require('moment');
+var moment = require('moment-timezone');
 
 const firebasedb = require('./setupFirebase');
+const createDateObj = (dateString, zone) => {
+    const dateTime = moment.tz(dateString, 'ddd, MMM D YYYY, h:mm A', zone).format();
+    return moment(dateTime).utc().unix();
+};
 
 const getMemberList = (number) => {
     return firebasedb.ref(`moc_by_congress/${number}`).once('value')
@@ -16,57 +20,67 @@ const getMemberList = (number) => {
 };
 
 function addToArchive(congress115, congress116) {
-    const paths = [];
+    let total = 0;
+    const promises = [];
     let path;
-    firebasedb.ref('archive_115th_congress').set(null);
-    firebasedb.ref('archive_116th_congress').set(null);
-    firebasedb.ref('townHallsOld').once('value')
-    .then((snapshot) => {
 
+    firebasedb.ref('archive_clean').once('value')
+    .then((snapshot) => {
         snapshot.forEach( (dateSnapshot) => {
             dateSnapshot.forEach(function(eventSnapShot) {
                 let archivePath;
                 var townHall = eventSnapShot.val();
-                let eventDate = moment(townHall.dateString);
-                if (includes(congress116, townHall.govtrack_id) && eventDate.isValid && eventDate.isSameOrAfter('2019-01-03')){
-                    archivePath = 'archive_116th_congress';      
-                } else if (includes(congress115, townHall.govtrack_id) ) {
-                    archivePath = 'archive_115th_congress';
-                } else if (townHall.thp_id && townHall.state) {
-                    archivePath = `state_townhalls_archive/${townHall.state}`;
-                } else {
-                    console.log('not in any congress', townHall.govtrack_id, townHall.thp_id, eventSnapShot.key);
-                    archivePath = 'no_member_id';
+                let eventDate;
+                try {
+                    eventDate = moment(townHall.dateString);
+                } catch (error) {
+
                 }
-                if (!townHall.eventId || !archivePath) {
-                    return;
+                const isAfterNewCongress = eventDate && eventDate.isValid && eventDate.isSameOrAfter('2019-01-03');
+                const isBeforeNewCongress = eventDate && eventDate.isValid && eventDate.isBefore('2019-01-03');
+                const isInNewCongress = townHall.govtrack_id && includes(congress116, townHall.govtrack_id);
+                const isInOldCongress = townHall.govtrack_id && includes(congress115, townHall.govtrack_id);
+
+                const isStateEvent = !!townHall.thp_id && townHall.thp_id.split('-').length > 3;
+                if (isStateEvent) {
+                    archivePath = 'state_fixes';
+                } else if (isAfterNewCongress || (townHall.repeatingEvent && isInNewCongress)) {
+                    archivePath = 'archive_116th_congress_session';
                 }
-                if (townHall.repeatingEvent) {
-                    console.log('repeating event', archivePath)
-                    path = `${archivePath}/repeating_event`;
-                } else if (townHall.dateString) {
-                    let eventDate = moment(townHall.dateString);
-                    if (eventDate.isValid()) {
+                else if (isBeforeNewCongress || (townHall.repeatingEvent && isInOldCongress)) {
+                    archivePath = 'archive_115th_congress_session';
+                } 
+                else {
+                    archivePath = 'archive_other';
+                }
+                
+                if (townHall.eventId && archivePath) {
+            
+                    if (townHall.repeatingEvent) {
+                        path = `${archivePath}/repeating_event`;
+                    } else if (eventDate.isValid()) {
                         var year = eventDate.get('year');
                         var month = eventDate.get('month');
                         var dateKey = year + '-' + month;
                         if (year.toString().length === 4) {
                             path = `${archivePath}/${dateKey}`;
-                        } else {
-                            path = `${archivePath}/date_error`;
-
                         }
+                    } else {
+                        path =`${archivePath}/no_date`;
                     }
+                    total = total + 1;
+                    promises.push(firebasedb.ref(`${path}/${townHall.eventId}`).update(townHall));
+                    
                 } else {
-                    path =`${archivePath}/no_date`;
+                    console.log('no path', dateSnapshot.key, townHall.eventId, townHall.thp_id)
                 }
-                return firebasedb.ref(`${path}/${townHall.eventId}`).update(townHall);
 
             });
         });
     })
-    .then(()=> {
-        console.log('done');
+    .then(() => {
+        Promise.all(promises)
+            .then(() => console.log('done'))
     })
     .catch(console.log);
 }
