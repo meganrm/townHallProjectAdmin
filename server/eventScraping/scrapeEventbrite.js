3d2c1e65817e431f67b6fb370c9098de7b38fd31
meganrm <meganrm@gmail.com>
2019-04-12 16:01:22 -0700

    eventbrite events use displayName instead of Member

--- server/eventScraping/scrapeEventbrite.js
+++ server/eventScraping/scrapeEventbrite.js
@@ -7,59 +7,58 @@ const statesAb = require('../data/stateMap');
 const eventBriteModule = {};
 
 eventBriteModule.createEventbriteQuery = (MoC, startDate) => {
-  return request({
-    uri: 'https://www.eventbriteapi.com/v3/organizers/' + MoC.eventbrite_id + '/events/?start_date.range_start=' + startDate +'&token=' + eventbriteToken,
-    json: true,
-  }).then(res => {
+    return request({
+        uri: 'https://www.eventbriteapi.com/v3/organizers/' + MoC.eventbrite_id + '/events/?start_date.range_start=' + startDate +'&token=' + eventbriteToken,
+        json: true,
+    }).then(res => {
     // Create references to MoCs for easy data lookup later
-    res.events.forEach(evnt => evnt.MoC = MoC);
-    return res.events || [];
-  }).catch(() => {});
+        res.events.forEach(evnt => evnt.MoC = MoC);
+        return res.events || [];
+    }).catch(() => {});
 };
 
 eventBriteModule.transformEventbriteTownhall = (eventBriteEvent) => {
-  let chamber;
-  if (eventBriteEvent.MoC.type === 'sen') {
-    chamber = 'upper';
-  } else {
-    chamber = 'lower';
-  }
-  let start = moment(eventBriteEvent.start.local);
-  let end = moment(eventBriteEvent.end.local);
-  var townhall = {
-    eventId: 'eb_' + eventBriteEvent.id,
-    Member: eventBriteEvent.MoC.displayName,
-    govtrack_id: eventBriteEvent.MoC.govtrack_id,
-    party: eventBriteEvent.MoC.party,
-    chamber: chamber,
-    district: eventBriteEvent.MoC.district,
-    State: statesAb[eventBriteEvent.MoC.state],
-    stateName: statesAb[eventBriteEvent.MoC.state],
-    state: eventBriteEvent.MoC.state,
-    eventName: eventBriteEvent.name.text,
-    meetingType: 'unknown',
-    link: eventBriteEvent.url,
-    linkName: 'Eventbrite RSVP',
-    dateObj: moment(eventBriteEvent.start.utc).valueOf(),
-    dateString: start.format('ddd, MMM D, YYYY'),
-    Date: start.format('ddd, MMM D, YYYY'),
-    Time: start.format('LT'),
-    timeStart24: start.format('HH:mm:ss'),
-    timeEnd24: end.format('HH:mm:ss'),
-    yearMonthDay: start.format('YYYY-MM-DD'),
-    lastUpdated: Date.now(),
-  };
-
-  if (eventBriteEvent.hasOwnProperty('venue')) {
-    townhall.Location = eventBriteEvent.venue.name;
-    townhall.lat = eventBriteEvent.venue.latitude;
-    townhall.lng = eventBriteEvent.venue.longitude;
-    if (eventBriteEvent.venue.hasOwnProperty('address')) {
-      townhall.address = eventBriteEvent.venue.address.localized_address_display;
+    let chamber;
+    if (eventBriteEvent.MoC.type === 'sen') {
+        chamber = 'upper';
+    } else {
+        chamber = 'lower';
     }
-  }
+    let start = moment(eventBriteEvent.start.local);
+    let end = moment(eventBriteEvent.end.local);
+    var townhall = {
+        eventId: 'eb_' + eventBriteEvent.id,
+        displayName: eventBriteEvent.MoC.displayName,
+        govtrack_id: eventBriteEvent.MoC.govtrack_id,
+        party: eventBriteEvent.MoC.party,
+        chamber: chamber,
+        district: eventBriteEvent.MoC.district,
+        stateName: statesAb[eventBriteEvent.MoC.state],
+        state: eventBriteEvent.MoC.state,
+        eventName: eventBriteEvent.name.text,
+        meetingType: 'unknown',
+        link: eventBriteEvent.url,
+        linkName: 'Eventbrite RSVP',
+        dateObj: moment(eventBriteEvent.start.utc).valueOf(),
+        dateString: start.format('ddd, MMM D, YYYY'),
+        Date: start.format('ddd, MMM D, YYYY'),
+        Time: start.format('LT'),
+        timeStart24: start.format('HH:mm:ss'),
+        timeEnd24: end.format('HH:mm:ss'),
+        yearMonthDay: start.format('YYYY-MM-DD'),
+        lastUpdated: Date.now(),
+    };
 
-  return townhall;
+    if (eventBriteEvent.hasOwnProperty('venue')) {
+        townhall.Location = eventBriteEvent.venue.name;
+        townhall.lat = eventBriteEvent.venue.latitude;
+        townhall.lng = eventBriteEvent.venue.longitude;
+        if (eventBriteEvent.venue.hasOwnProperty('address')) {
+            townhall.address = eventBriteEvent.venue.address.localized_address_display;
+        }
+    }
+
+    return townhall;
 };
 
 module.exports = eventBriteModule;
