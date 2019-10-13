2f366920538c0e833f1f6a5c147ac33bcc09e80f
meganrm <meganrm@gmail.com>
2019-10-12 17:18:12 -0700

    delinting

--- server/moc/update-crisis-status.js
+++ server/moc/update-crisis-status.js
@@ -1,23 +1,23 @@
 const firebasedb = require('../lib/setupFirebase');
 
 const convertDataAndUpdate = (row) => {
-  const govtrackId = row[0];
-  const crisis_status = row[5].match(/\d/g) ? row[5].match(/\d/g)[0] : NaN;
-  if (isNaN(govtrackId) || isNaN(crisis_status)) {
-    return;
-  }
+    const govtrackId = row[0];
+    const crisis_status = row[5].match(/\d/g) ? row[5].match(/\d/g)[0] : NaN;
+    if (isNaN(govtrackId) || isNaN(crisis_status)) {
+        return;
+    }
 
-  const moc = {
-    crisis_status: Number(crisis_status),
-    crisis_status_source: row[6] || '',
-  };
-  const ref = firebasedb.ref(`mocData/${govtrackId}`);
+    const moc = {
+        crisis_status: Number(crisis_status),
+        crisis_status_source: row[6] || '',
+    };
+    const ref = firebasedb.ref(`mocData/${govtrackId}`);
 
-  ref.once('value')
+    ref.once('value')
         .then((snapshot) => {
-          if (snapshot.exists()) {
-            ref.update(moc);
-          }
+            if (snapshot.exists()) {
+                ref.update(moc);
+            }
         });
 };
 
